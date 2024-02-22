504be7778455d3fdae6c1e2bb559b4b02fe048f1
whoischrislam <whoischrislam@gmail.com>
2024-02-21 17:00:09 -1000

    Adding CSS and JS

---
+++ index.js
@@ -0,0 +1,9 @@
+const saveBTN = document.getElementById("input-btn");
+
+function saveInput() {
+    console.log("Button clicked");
+}
+
+saveBTN.addEventListener("click", () => {
+    saveInput();
+});
