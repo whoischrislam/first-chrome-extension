43d4ca9404a135053180853b2463f83994e6f2a3
whoischrislam <whoischrislam@gmail.com>
2024-02-22 21:15:53 -1000

    progress

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const saveBTN = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 let myLeads = [];
+const ulEl = document.getElementById("ul-el");
 
 saveBTN.addEventListener("click", saveInput);
 
 function saveInput() {
     myLeads.push(inputEl.value);
-    showLeads();
+    ulEl.innerHTML += `<li>${inputEl.value}</li>`;
+    console.log(`Current Array: ${myLeads}`);
 }
 
-function showLeads() {
-    console.log("myLeads: ");
-    for (let i = 0; i < myLeads.length; i++) {
-        console.log(myLeads[i]);
-    }
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log("rendering: " + myLeads[i]);
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
 }
+console.log(myLeads);
