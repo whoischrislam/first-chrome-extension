571fc9e391412e38f3cd04caf4dc000f4f9b88f6
whoischrislam <whoischrislam@gmail.com>
2024-02-22 15:48:35 -1000

    another commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const saveBTN = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+let myLeads = [];
+
+saveBTN.addEventListener("click", saveInput);
 
 function saveInput() {
-    console.log("Button clicked");
+    myLeads.push(inputEl.value);
+    showLeads();
 }
 
-saveBTN.addEventListener("click", () => {
-    saveInput();
-});
+function showLeads() {
+    console.log("myLeads: ");
+    for (let i = 0; i < myLeads.length; i++) {
+        console.log(myLeads[i]);
+    }
+}
